94866508bef9a349d8ba636a4c486612c8a98a53
PIRANY <121549049+PIRANY1@users.noreply.github.com>
2023-10-06 23:08:13 +0200

    Update start.bat
    untested migration

--- start.bat
+++ start.bat
@@ -103,5 +103,70 @@ If %menu3% == 2 goto done
 If %menu3% == 1 start "" "https://github.com/PIRANY1/DataSpammer"
 
 :done 
-set "noerror=1000"
-startupcheck.bat
+
+rem ------------------------------------------------------------
+
+:PARTSTUPCCK
+setlocal enabledelayedexpansion
+set "error=0"
+color 02
+echo Checking for Data...
+:noerr
+if not exist "stdrcch.txt" (
+    goto Error
+) else (
+    if not exist "stdfil.txt" (
+    goto Error
+    ) else (
+        if not exist "instdone.txt" (
+        goto Error
+        ) else (
+            goto start   
+        )  
+    ) 
+)
+
+:error 
+cls
+echo There was an Error. 
+@ping -n 1 localhost> nul
+echo Please consider rerunning the installer to make sure the script can run accurately.
+@ping -n 1 localhost> nul
+echo Do you want to reinstall the Script or do you want to cancel?
+@ping -n 1 localhost> nul
+echo.
+@ping -n 1 localhost> nul
+echo [1] Open the Installer
+@ping -n 1 localhost> nul
+echo.
+@ping -n 1 localhost> nul
+echo [2] Cancel
+@ping -n 1 localhost> nul
+echo.
+@ping -n 1 localhost> nul
+echo [3] Open the Script anyways
+@ping -n 1 localhost> nul
+set /p menu=Choose an Option from Above:
+If %menu% == 1 goto openins
+If %menu% == 2 exit
+If %menu% == 3 goto openanyways
+
+:openins
+cd %~dp0
+install.bat
+if %errorlevel% equ 0 (
+    cls
+    echo Installer is running....
+    cls
+) else (
+    echo There was an Error. Please open the install.bat File manually.
+)
+
+:start
+set "noerror2=10000"
+servercrasher.bat
+
+:openanyways
+set "noerror2=10000"
+servercrasher.bat
+
